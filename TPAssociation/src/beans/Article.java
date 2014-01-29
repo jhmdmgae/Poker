c1147ca00f32cd39d461fca4999108c2d9dcc8ee
ACOUINEAU <alexandre.couineau@hotmail.fr>
2014-01-29 18:08:27 +0100

    modif de la classe Article dans le bean et ArticleServices dans service

--- TPAssociation/src/beans/Article.java
+++ TPAssociation/src/beans/Article.java
@@ -1,21 +1,32 @@
 package beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.*;
 
+@Entity
+@Table(name="ARTICLE", schema="ASSOC_EMN")
 public class Article{
 	
+	@Id
+	@Column(name="CODE")
 	@NotNull
 	@Size(min = 1, max = 10)
 	private String code; 
 	
+	@Column(name="NOM")
 	@NotNull
 	@Size(min = 1, max = 10)
 	private String nom;
 	
+	@Column(name="PRIX")
 	@NotNull
 	@Min(1)
 	private double prix;
 	
+	@Column(name="QUANTITE")
 	@NotNull
 	@Min(0)
 	private int quantite;
--- TPAssociation/src/services/ArticleServices.java
+++ TPAssociation/src/services/ArticleServices.java
@@ -3,31 +3,40 @@ package services;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import beans.Article;
+
+import java.util.List;
 
 public class ArticleServices {
 	private EntityManagerFactory emf;
 	private EntityManager em;
-	
+
 	public ArticleServices(EntityManagerFactory emf, EntityManager em) {
 		emf = Persistence.createEntityManagerFactory("Assoc_EMN");
 		em = emf.createEntityManager();
 	}
-	
-	public Article find(String code){
-		return em.find(Article.class,code);
+
+	public Article find(String code) {
+		return em.find(Article.class, code);
 	}
-	
-	public void create(Article u){
+
+	public void create(Article u) {
 		em.getTransaction().begin();
 		em.persist(u);
 		em.getTransaction().commit();
 	}
-	
-	public void close(){
+
+	public List<Article> listAll() {
+		Query req = em.createQuery("Select a from ARTICLES a");
+		List<Article> listArticle = req.getResultList();
+		return listArticle;
+	}
+
+	public void close() {
 		em.close();
 		emf.close();
 	}
-	
+
 }
