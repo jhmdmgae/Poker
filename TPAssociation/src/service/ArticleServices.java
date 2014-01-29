42fa5cd3e31dfcdc0d0520e1e5348e4b258b27ee
ACOUINEAU <alexandre.couineau@hotmail.fr>
2014-01-29 17:30:01 +0100

    commit de ArticleServices dans le package service

---
+++ TPAssociation/src/service/ArticleServices.java
@@ -0,0 +1,33 @@
+package service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import beans.Article;
+
+public class ArticleServices {
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	
+	public ArticleServices(EntityManagerFactory emf, EntityManager em) {
+		emf = Persistence.createEntityManagerFactory("Assoc_EMN");
+		em = emf.createEntityManager();
+	}
+	
+	public Article find(String code){
+		return em.find(Article.class,code);
+	}
+	
+	public void create(Article u){
+		em.getTransaction().begin();
+		em.persist(u);
+		em.getTransaction().commit();
+	}
+	
+	public void close(){
+		em.close();
+		emf.close();
+	}
+	
+}
