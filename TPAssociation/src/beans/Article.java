96c8b3c2091301c1fc88b6b00d196291c2eeed4b
ACOUINEAU <alexandre.couineau@hotmail.fr>
2014-01-29 16:40:15 +0100

    commit du bean Article

---
+++ TPAssociation/src/beans/Article.java
@@ -0,0 +1,59 @@
+package beans;
+
+public class Article {
+
+	private String code; 
+	private String nom;
+	private double prix;
+	private int quantite;
+	
+	
+	public Article() {
+		
+	}
+
+
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public double getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
+	
+	
+	
+	
+}
